8f2647eee8a20494a092e93378f9bf9822a539bf
Alicia Weenum <weenum.alicia@gmail.com>
2021-02-08 18:54:02 -0500

    commit tree 8c6678f764aed3848e89e903710220de2593e5d0

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia']
+
+function returnFirstTwoDrivers(){
+    return ['Sally', 'Bob']
+}
+
+
+function returnLastTwoDrivers(){
+    return [ 'Freddy', 'Claudia' ]
+}
+
+
+// const returnFirstTwoDrivers = function (drivers) {
+//     return drivers.slice(0, 2);
+//   };
+  
+//   const returnLastTwoDrivers = function (drivers) {
+//     return drivers.slice(-2);
+//   };
+  
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function(multiplier){
+    return function(value){
+        return multiplier * value
+    }
+
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+
+const selectDifferentDrivers = function (drivers, whichDrivers) {
+    return whichDrivers(drivers);
+  };
